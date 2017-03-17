b205d9eaf0f9f5438e68827f797d7eff38e37373
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-17 20:23:21 +1300

    add css to the qty and addToCart btn

--- application/views/products.php
+++ application/views/products.php
@@ -9,10 +9,16 @@
       <div class="game-title">
         <?php echo $product->title; ?>
       </div>
+
       <div class="game-add">
-        <button class="btn btn-primary" type="submit">Add To Carts</button>
+        <form method="post" action="<?php echo base_url();?>cart/add">
+          QTY:<input class="qty" type="text" name="qty" value="1" />
+          <input type="hidden" name="item_number" value="<?php echo $product->id; ?>" />
+          <input type="hidden" name="price" value="<?php echo $product->price; ?>" />
+          <input type="hidden" name="title" value="<?php echo $product->title;?>" />
+          <button class="btn btn-primary" type="submit">Add To Cart</button>
+        </form>
       </div>
   </div>
-
 
 <?php endforeach; ?>
