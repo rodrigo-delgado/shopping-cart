72e101f9c83d24a983216d29659592504ec183c1
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-17 20:01:19 +1300

    add products into view

--- application/views/products.php
+++ application/views/products.php
@@ -1,3 +1,18 @@
 <?php foreach ($products as $product) : ?>
-  <?php echo $product->title; ?><br />
+
+  <div class="col-md-4 game">
+    <div class="game-price"><?php echo $product->price; ?></div>
+      <a href="<?php echo base_url(); ?>products/details/<?php echo $product->id; ?>">
+        <img class="img-responsive" src="<?php echo base_url(); ?>assets/images/products/<?php echo $product->image; ?>">
+      </a>
+
+      <div class="game-title">
+        <?php echo $product->title; ?>
+      </div>
+      <div class="game-add">
+        <button class="btn btn-primary" type="submit">Add To Carts</button>
+      </div>
+  </div>
+
+
 <?php endforeach; ?>
