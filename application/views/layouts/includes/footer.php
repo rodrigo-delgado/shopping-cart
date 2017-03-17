e4570ef40a95404fd12a3ab1f281525648e36a88
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-17 16:30:53 +1300

    add footer and sidebar

---
+++ application/views/layouts/includes/footer.php
@@ -0,0 +1,21 @@
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="footer">
+  <div class="container">
+    <h1>The gaming place</h1>
+      <p>footer here</p>
+  </div>
+</div>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="js/bootstrap.js"></script>
+
+</body>
+</html>
--- application/views/layouts/includes/header.php
+++ application/views/layouts/includes/header.php
@@ -48,58 +48,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-4">
-          <div class="cart-block">
-            <form action="cart/update" method="post">
-              <table cellpadding="6" cellspacing="1" style="width:100%" border="0">
-                <tr>
-                  <th>QTY</th>
-                  <th>Item Description</th>
-                  <th style="text-align:right">Item Price</th>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td class="right"><strong>Total</strong></td>
-                  <td class="right" style="text-align:right">$</td>
-                </tr>
-              </table><br />
-              <p><button class="btn btn-default" type="submit">Update Cart</button>
-              <a class="btn btn-default" href="cart">Go To Cart</a></p>
-            </form>
-          </div>
-
-        <div class="panel panel-default panel-list">
-          <div class="panel-heading panel-heading-dark">
-            <h3 class="panel-title">
-              Categories
-            </h3>
-          </div>
-
-          <ul class="list-group">
-            <li class="list-group-item"><a href="#">Gaming Consoles</a></li>
-            <li class="list-group-item"><a href="#">Xbox One Games</a></li>
-            <li class="list-group-item"><a href="#">Playstation 4 games</a></li>
-            <li class="list-group-item"><a href="#">Xbox 360 Games</a></li>
-            <li class="list-group-item"><a href="#">Nintendo Wii Games</a></li>
-            <li class="list-group-item"><a href="#">Playstation 3 games</a></li>
-            <li class="list-group-item"><a href="#">Other Console Games</a></li>
-          </ul>
-        </div>
-          <div class="panel panel-default panel-list">
-            <div class="panel-heading panel-heading-dark">
-              <h3 class="panel-title">
-                Most Populars
-              </h3>
-            </div>
-
-            <ul class="list-group">
-              <li class="list-group-item"><a href="#">Gaming Consoles</a></li>
-              <li class="list-group-item"><a href="#">Xbox One Games</a></li>
-              <li class="list-group-item"><a href="#">Playstation 4 games</a></li>
-              <li class="list-group-item"><a href="#">Xbox 360 Games</a></li>
-              <li class="list-group-item"><a href="#">Nintendo Wii Games</a></li>
-              <li class="list-group-item"><a href="#">Playstation 3 games</a></li>
-              <li class="list-group-item"><a href="#">Other Console Games</a></li>
-            </ul>
+          <?php $this->load->view('layouts/includes/sidebar'); ?>
           </div>
         </div>
 
@@ -108,9 +57,7 @@
         <div class="col-md-8">
           <div class="panel panel-default">
             <div class="panel-heading panel-heading-green">
-              <h3 class="panel-title">
-                Latest Realeses
-              </h3>
+              <h3 class="panel-title">This is the Gaming Place (for now)</h3>
             </div>
 
               <div class="panel-body">
